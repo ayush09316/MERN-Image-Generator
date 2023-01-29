95150a18ac26a29880d384ff2843f6d5bb9c7ce1
Ayush Sharma <ayushsharma170199@gmail.com>
2023-01-29 23:49:55 +0530

    origin cors added

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const Port=8080;
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:'https://dalle-e-image-generator.netlify.app',
+}));
 app.use(express.json({ limit: '50mb' }));
 // const stripe=require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
